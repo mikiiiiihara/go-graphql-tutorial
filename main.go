b771d52552fe8de4bbe5731be27b84431dc9ba7b
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-05-09 01:52:02 +0900

    rest apiのラッピング処理実装

--- main.go
+++ main.go
@@ -5,20 +5,14 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/graphql-go/graphql"
+	"github.com/mikiiiiihara/go-graphql-tutorial/resolver"
+
 	"github.com/graphql-go/handler"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-type User struct {
-	ID       uint   `json:"id"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"-"`
-}
-
-var users []User
+var users []resolver.User
 var db *gorm.DB
 
 func main() {
@@ -29,137 +23,14 @@ func main() {
 		panic("failed to connect database")
 	}
 
-	db.AutoMigrate(&User{})
+	db.AutoMigrate(&resolver.User{})
 
-	userType := graphql.NewObject(graphql.ObjectConfig{
-		Name: "User",
-		Fields: graphql.Fields{
-			"id": &graphql.Field{
-				Type: graphql.Int,
-			},
-			"name": &graphql.Field{
-				Type: graphql.String,
-			},
-			"email": &graphql.Field{
-				Type: graphql.String,
-			},
-		},
-	})
+	resolver := resolver.NewResolver(db)
+	schema, err := resolver.CreateSchema()
+	if err != nil {
+		log.Fatalf("failed to create new schema, error: %v", err)
+	}
 
-	rootQuery := graphql.NewObject(graphql.ObjectConfig{
-		Name: "Query",
-		Fields: graphql.Fields{
-			// Query to get all users
-			"users": &graphql.Field{
-				Type: graphql.NewList(userType),
-				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-					db.Find(&users)
-					return users, nil
-				},
-			},
-			// Query to get a user by ID
-			"user": &graphql.Field{
-				Type: userType,
-				Args: graphql.FieldConfigArgument{
-					"id": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.Int),
-					},
-				},
-				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-					id := p.Args["id"].(int)
-					var user User
-					db.First(&user, id)
-					return user, nil
-				},
-			},
-		},
-	})
-	rootMutation := graphql.NewObject(graphql.ObjectConfig{
-		Name: "Mutation",
-		Fields: graphql.Fields{
-			"createUser": &graphql.Field{
-				Type: userType,
-				Args: graphql.FieldConfigArgument{
-					"name": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-					"email": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-					"password": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-				},
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					user := User{
-						Name:     params.Args["name"].(string),
-						Email:    params.Args["email"].(string),
-						Password: params.Args["password"].(string),
-					}
-					db.Create(&user)
-					return user, nil
-				},
-			},
-			"updateUser": &graphql.Field{
-				Type: userType,
-				Args: graphql.FieldConfigArgument{
-					"id": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.Int),
-					},
-					"name": &graphql.ArgumentConfig{
-						Type: graphql.String,
-					},
-					"email": &graphql.ArgumentConfig{
-						Type: graphql.String,
-					},
-					"password": &graphql.ArgumentConfig{
-						Type: graphql.String,
-					},
-				},
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					id := params.Args["id"].(int)
-					var user User
-					db.First(&user, id)
-	
-					if name, ok := params.Args["name"].(string); ok {
-						user.Name = name
-					}
-	
-					if email, ok := params.Args["email"].(string); ok {
-						user.Email = email
-					}
-	
-					if password, ok := params.Args["password"].(string); ok {
-						user.Password = password
-					}
-	
-					db.Save(&user)
-					return user, nil
-				},
-			},
-			"deleteUser": &graphql.Field{
-				Type: userType,
-				Args: graphql.FieldConfigArgument{
-					"id": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.Int),
-					},
-				},
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					id := params.Args["id"].(int)
-					var user User
-					db.First(&user, id)
-					db.Delete(&user)
-					return user, nil
-				},
-			},
-		},
-	})	
-
-	schema, _ := graphql.NewSchema(graphql.SchemaConfig{
-		Query: rootQuery,
-		Mutation: rootMutation,
-	})
-	// GraphQLハンドラーを設定
 	h := handler.New(&handler.Config{
 		Schema:   &schema,
 		Pretty:   true,
---
+++ resolver/resolver.go
@@ -0,0 +1,227 @@
+package resolver
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/graphql-go/graphql"
+	"gorm.io/gorm"
+)
+
+
+type Resolver struct {
+	db *gorm.DB
+}
+
+type User struct {
+	ID       uint   `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"-"`
+}
+
+type Ticker struct {
+	ProductCode      string  `json:"product_code"`
+	State            string  `json:"state"`
+	Timestamp        string  `json:"timestamp"`
+	TickID           int     `json:"tick_id"`
+	BestBid          float64 `json:"best_bid"`
+	BestAsk          float64 `json:"best_ask"`
+	BestBidSize      float64 `json:"best_bid_size"`
+	BestAskSize      float64 `json:"best_ask_size"`
+	TotalBidDepth    float64 `json:"total_bid_depth"`
+	TotalAskDepth    float64 `json:"total_ask_depth"`
+	MarketBidSize    float64 `json:"market_bid_size"`
+	MarketAskSize    float64 `json:"market_ask_size"`
+	Ltp              float64 `json:"ltp"`
+	Volume           float64 `json:"volume"`
+	VolumeByProduct  float64 `json:"volume_by_product"`
+}
+
+func (r *Resolver) GetAllUsers(p graphql.ResolveParams) (interface{}, error) {
+	var users []User
+	r.db.Find(&users)
+
+	// コンソールにユーザーを出力
+	for _, user := range users {
+		fmt.Printf("ID: %d, Name: %s, Email: %s\n", user.ID, user.Name, user.Email)
+	}
+
+	return users, nil
+}
+
+func (r *Resolver) GetUserByID(p graphql.ResolveParams) (interface{}, error) {
+	id := p.Args["id"].(int)
+	var user User
+	r.db.First(&user, id)
+	return user, nil
+}
+
+
+func NewResolver(db *gorm.DB) *Resolver {
+	return &Resolver{db: db}
+}
+
+func (r *Resolver) CreateSchema() (graphql.Schema, error) {
+	userType := graphql.NewObject(graphql.ObjectConfig{
+		Name: "User",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.Int,
+			},
+			"name": &graphql.Field{
+				Type: graphql.String,
+			},
+			"email": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	},)
+
+	tickerType := graphql.NewObject(graphql.ObjectConfig{
+		Name: "Ticker",
+		Fields: graphql.Fields{
+			"product_code":      &graphql.Field{Type: graphql.String},
+			"state":             &graphql.Field{Type: graphql.String},
+			"timestamp":         &graphql.Field{Type: graphql.String},
+			"tick_id":           &graphql.Field{Type: graphql.Int},
+			"best_bid":          &graphql.Field{Type: graphql.Float},
+			"best_ask":          &graphql.Field{Type: graphql.Float},
+			"best_bid_size":     &graphql.Field{Type: graphql.Float},
+			"best_ask_size":     &graphql.Field{Type: graphql.Float},
+			"total_bid_depth":   &graphql.Field{Type: graphql.Float},
+			"total_ask_depth":   &graphql.Field{Type: graphql.Float},
+			"market_bid_size":   &graphql.Field{Type: graphql.Float},
+			"market_ask_size":   &graphql.Field{Type: graphql.Float},
+			"ltp":               &graphql.Field{Type: graphql.Float},
+			"volume":            &graphql.Field{Type: graphql.Float},
+			"volume_by_product": &graphql.Field{Type: graphql.Float},
+		},
+	},)
+
+	rootQuery := graphql.NewObject(
+		graphql.ObjectConfig{
+		Name: "Query",
+		Fields: graphql.Fields{
+			"users": &graphql.Field{
+				Type:    graphql.NewList(userType),
+				Resolve: r.GetAllUsers,
+			},
+			"user": &graphql.Field{
+				Type: userType,
+				Args: graphql.FieldConfigArgument{
+					"id": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.Int),
+					},
+				},
+				Resolve: r.GetUserByID,
+			},
+			"cryptoTicker":&graphql.Field{
+				Type: tickerType,
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					resp, err := http.Get("https://api.bitflyer.com/v1/ticker")
+					if err != nil {
+						return nil, err
+					}
+					defer resp.Body.Close()
+
+					var ticker Ticker
+					err = json.NewDecoder(resp.Body).Decode(&ticker)
+					if err != nil {
+						return nil, err
+					}
+					return ticker, nil
+				},
+			},
+		},
+	})
+
+	rootMutation := graphql.NewObject(graphql.ObjectConfig{
+		Name: "Mutation",
+		Fields: graphql.Fields{
+			"createUser": &graphql.Field{
+				Type: userType,
+				Args: graphql.FieldConfigArgument{
+					"name": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.String),
+					},
+					"email": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.String),
+					},
+					"password": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.String),
+					},
+				},
+				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+					user := User{
+						Name:     params.Args["name"].(string),
+						Email:    params.Args["email"].(string),
+						Password: params.Args["password"].(string),
+					}
+					r.db.Create(&user)
+					return user, nil
+				},
+			},
+			"updateUser": &graphql.Field{
+				Type: userType,
+				Args: graphql.FieldConfigArgument{
+					"id": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.Int),
+					},
+					"name": &graphql.ArgumentConfig{
+						Type: graphql.String,
+					},
+					"email": &graphql.ArgumentConfig{
+						Type: graphql.String,
+					},
+					"password": &graphql.ArgumentConfig{
+						Type: graphql.String,
+					},
+				},
+				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+					id := params.Args["id"].(int)
+					var user User
+					r.db.First(&user, id)
+	
+					if name, ok := params.Args["name"].(string); ok {
+						user.Name = name
+					}
+	
+					if email, ok := params.Args["email"].(string); ok {
+						user.Email = email
+					}
+	
+					if password, ok := params.Args["password"].(string); ok {
+						user.Password = password
+					}
+	
+					r.db.Save(&user)
+					return user, nil
+				},
+			},
+			"deleteUser": &graphql.Field{
+				Type: userType,
+				Args: graphql.FieldConfigArgument{
+					"id": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.Int),
+					},
+				},
+				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+					id := params.Args["id"].(int)
+					var user User
+					r.db.First(&user, id)
+					r.db.Delete(&user)
+					return user, nil
+				},
+			},
+		},
+	})	
+
+	schemaConfig := graphql.SchemaConfig{
+		Query:    rootQuery,
+		Mutation: rootMutation,
+	}
+
+	return graphql.NewSchema(schemaConfig)
+}
